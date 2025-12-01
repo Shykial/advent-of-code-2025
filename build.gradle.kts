13987af1084adb3b15b58174c72e4e27e532b519
Szymon Jeziorski <szymon.jeziorski@outlook.com>
2025-12-01 10:17:43 +0100

    adjust template for local conventions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,77 @@
+import kotlinx.benchmark.gradle.BenchmarkConfiguration
+
 plugins {
-    kotlin("jvm") version "2.2.21"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.allopen)
+    alias(libs.plugins.kotlinx.benchmark)
+    alias(libs.plugins.aoc.inputs.downloader)
+    alias(libs.plugins.ktlint)
 }
 
-sourceSets {
-    main {
-        kotlin.srcDir("src")
+repositories {
+    mavenCentral()
+}
+
+allOpen {
+    annotation("org.openjdk.jmh.annotations.State")
+}
+
+val benchmarksSourceSet = sourceSets.create("benchmarks")
+
+kotlin {
+    jvmToolchain(21)
+    target {
+        compilations["benchmarks"].associateWith(compilations["main"])
     }
 }
 
-tasks {
-    wrapper {
-        gradleVersion = "9.2.1"
+val benchmarksDirectorySubPath = File.separator + benchmarksSourceSet.name + File.separator
+
+ktlint {
+    filter {
+        exclude { benchmarksDirectorySubPath in it.file.absolutePath }
+    }
+}
+
+val benchmarksImplementation by configurations
+
+dependencies {
+    benchmarksImplementation(libs.kotlinx.benchmark.runtime)
+}
+
+downloadAocInputs {
+    sessionCookie { projectDir.resolve("secrets/session-cookie.txt").readText() }
+}
+
+benchmark {
+    targets.register("benchmarks")
+    configurations {
+        benchmarksSourceSet.kotlin.asSequence()
+            .filter { it.extension == "kt" }
+            .forEach { file -> registerBenchmarkTasks(file.nameWithoutExtension) }
+    }
+}
+
+fun NamedDomainObjectContainerScope<BenchmarkConfiguration>.registerBenchmarkTasks(fileName: String) {
+    val trimmedName = fileName.replaceFirstChar { it.lowercaseChar() }.substringBefore("Benchmark")
+    val includePattern = ".*$fileName.*"
+
+    register("${trimmedName}Standard") {
+        include(includePattern)
+        warmups = 5
+        iterations = 5
+        iterationTime = 5
+        iterationTimeUnit = "sec"
+        mode = "avgt"
+        outputTimeUnit = "ms"
+    }
+    register("${trimmedName}Fast") {
+        include(includePattern)
+        warmups = 20
+        iterations = 5
+        iterationTime = 500
+        iterationTimeUnit = "ms"
+        mode = "avgt"
+        outputTimeUnit = "ms"
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,19 @@
-rootProject.name = "advent-of-code-2025"
-
-dependencyResolutionManagement {
+pluginManagement {
     repositories {
-        mavenCentral()
+        gradlePluginPortal()
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Shykial/aoc-inputs-downloader")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_PACKAGES_TOKEN")
+            }
+        }
     }
 }
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+rootProject.name = "advent-of-code-2025"
